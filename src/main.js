8cbd68d3209ca50653f6f3a05c32552926839850
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-03-24 19:52:15 -0700

    remove tomogatchi code

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
 import './backEnd.js';
 import $ from 'jquery';
-import {Tamagotchi, age} from "./backEnd.js";
+import {age} from "./backEnd.js";
 
 
 
@@ -17,45 +17,5 @@ $(document).ready(function() {
     console.log(normalAge);
 
 
-  });
-
-  $('.nameIt').submit(function(event) {
-    event.preventDefault();
-    var name = $('input#name-input').val();
-    $(".nameIt").hide();
-    $('.name-out').text(name + " " + "LIVES!");
-    $('.firstBorn').fadeIn(2000);
-    const bongo = new Tamagotchi();
-    let hungryIntervalId = bongo.hungry();
-    let sleepIntervalId = bongo.sleepy();
-    let boredIntervalId = bongo.bored();
-    bongo.death(hungryIntervalId, sleepIntervalId, boredIntervalId);
-    bongo.checkNumbers();
-    bongo.sleepBar();
-    bongo.foodBar();
-    bongo.funBar();
-
-    $('#feedBut').click(function() {
-      bongo.feed();
-      console.log(bongo.food)
-    });
-    $('#playBut').click(function() {
-      bongo.play();
-      console.log(bongo.attention)
-    });
-    $('#sleepBut').click(function() {
-      bongo.rest();
-      console.log(bongo.sleep);
-    });
-    $('#pickOn').click(function(){
-      bongo.pickOn();
-    })
-    $('#inject').click(function(){
-      bongo.inject();
-    })
-    $('.killBut').click(function() {
-      bongo.kill();
-      console.log(bongo.food)
-    });
   });
 });
